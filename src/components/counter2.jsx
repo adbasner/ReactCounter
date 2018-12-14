2ca6b18be1f45b14d8eeb727fded5c02d9b56226
Andrew Basner <andrewbasner@gmail.com>
2018-12-14 11:02:25 -0600

    initial commit, added increment button

---
+++ src/components/counter2.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Counter2 extends Component {
+  state = {
+    count: 1,
+    tags: ['tag1', 'tag2', 'tag3']
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>'There are no tags!'</p>;
+
+    return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>;
+
+  }
+
+  render () {
+    return (
+      <div>
+        { this.renderTags() }
+
+      </div>
+    );
+  }
+}
+
+export default Counter2;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import ReactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './components/counter';
+// import Counter2 from './components/counter2';
+ReactDom.render(<Counter />, document.getElementById('root'));
+// ReactDom.render(<Counter2 />, document.getElementById('root'));
